76d8d3f07c3f54444bb12d203cf334fcc1d79dd5
HulaMR <90249496+HulaMR@users.noreply.github.com>
2022-06-18 00:47:56 +0300

    Google Auth

---
+++ src/oauth/endpoint/views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets, parsers, permissions
+from .. import serializer, models
+from ...base.permissions import IsAuthor
+
+
+class UserView(viewsets.ModelViewSet):
+    parser_classes = (parsers.MultiPartParser,)
+    serializer_class = serializer.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user
+
+    def get_object(self):
+        return self.get_queryset()
+
+
+class AuthorView(viewsets.ReadOnlyModelViewSet):
+    queryset = models.AuthUser.objects.all().prefetch_related('social_links')
+    serializer_class = serializer.AuthorSerializer
+
+
+class SocialLinkView(viewsets.ModelViewSet):
+    serializer_class = serializer.SocialLinkSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return self.request.user.social_links.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
